21cdf511e9df7ed853a150001925529848df150c
javargas1098 <ja.vargas1098@gmail.com>
2019-11-08 12:48:52 -0500

    falata rticulo

---
+++ src/main/java/edu/eci/arem/ActiveMQ/App.java
@@ -0,0 +1,13 @@
+package edu.eci.arem.ActiveMQ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
